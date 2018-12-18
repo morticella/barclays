c45a2c34110fdd70c3bbc3e05936555e58e5ac6e
morticella <info@morticella.com>
2018-12-18 19:49:45 +0100

    Add dynamic AZ filter text

--- src/app/config/table/table.component.ts
+++ src/app/config/table/table.component.ts
@@ -12,22 +12,27 @@ export class TableComponent implements OnInit {
   bases: any[];
   currency: string;
   controlAZ: boolean;
+  textFilter: string;
+
+
 
   constructor(private configService: ConfigService) { }
 
   ngOnInit() {
     this.bases = this.configService.tableData;
     this.currency = this.configService.currency;
+    this.textFilter = 'A-Z';
   }
 
   sortByAZ() {
-    console.log('control', this.controlAZ);
     if (!this.controlAZ) {
       this.bases.sort((a, b) => a.curr.localeCompare(b.curr));
       this.controlAZ = true;
+      this.textFilter = 'A-Z';
     } else {
       this.bases.reverse();
       this.controlAZ = false;
+      this.textFilter = 'Z-A';
     }
   }
 

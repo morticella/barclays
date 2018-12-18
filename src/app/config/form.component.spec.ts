2e10177bd6339faf571ca0bd0e5fb33b9dcc00ec
morticella <info@morticella.com>
2018-12-18 22:58:11 +0100

    just some little improvement tomorrow I will work on it, I'm too tired

--- src/app/config/form.component.spec.ts
+++ src/app/config/form.component.spec.ts
@@ -3,19 +3,19 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { FormComponent } from './form.component';
-// import { ConfigService } from './config.service';
+import { ConfigService } from './config.service';
 
 describe('FormComponent', () => {
 
   let component: FormComponent;
   let fixture: ComponentFixture<FormComponent>;
+  let configService: ConfigService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
         ReactiveFormsModule,
-        HttpClientTestingModule,
-        // ConfigService
+        HttpClientTestingModule
       ],
       declarations: [ FormComponent ]
     })
@@ -26,8 +26,16 @@ describe('FormComponent', () => {
     fixture = TestBed.createComponent(FormComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
 
+  });
+  it('should update bases in component', () => {
+    fixture = TestBed.createComponent(FormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    configService = fixture.debugElement.injector.get(ConfigService) ;
+    expect (configService.tableData).toEqual(component.bases);
+    console.log(component.getRates.get('bases').value);
+  });
   it('should create', () => {
     expect(component).toBeTruthy();
   });

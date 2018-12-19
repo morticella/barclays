f6a719b8356e39a5991e0ad8f4b6698f853fdddb
morticella <info@morticella.com>
2018-12-19 18:03:12 +0100

    Spec Order by AZ/ZA

--- src/app/config/table/table.component.spec.ts
+++ src/app/config/table/table.component.spec.ts
@@ -1,18 +1,14 @@
-import { async, ComponentFixture, TestBed} from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { FormComponent } from './form.component';
-import { ConfigService } from './config.service';
+
+import { TableComponent } from './table.component';
+import { ConfigService, Config } from '../config.service';
 
 describe('FormComponent', () => {
-  let component: FormComponent;
-  let fixture: ComponentFixture<FormComponent>;
+  let component: TableComponent;
+  let fixture: ComponentFixture<TableComponent>;
   let configService: ConfigService;
-  const config = {
-    'base': 'GBP',
-    'date': '2018-10-28',
-    'rates': 1
-  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -21,36 +17,43 @@ describe('FormComponent', () => {
         HttpClientTestingModule,
 
       ],
-      declarations: [ FormComponent]
+      declarations: [ TableComponent]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(FormComponent);
+    fixture = TestBed.createComponent(TableComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
   it('should update bases in component', () => {
-    fixture = TestBed.createComponent(FormComponent);
+    fixture = TestBed.createComponent(TableComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
     configService = fixture.debugElement.injector.get(ConfigService) ;
     expect (configService.tableData).toEqual(component.bases);
-
-  });
-  it('check - 5%', () => {
-    fixture = TestBed.createComponent(FormComponent);
-    fixture.detectChanges();
-    configService.tableData[0].buy = config.rates - (config.rates / 100 * 5);
-    expect (configService.tableData[0].buy).toEqual(0.95);
   });
 
-  it('check + 5%', () => {
-    fixture = TestBed.createComponent(FormComponent);
+  it('should order A-Z', () => {
+    fixture = TestBed.createComponent(TableComponent);
+    component = fixture.componentInstance;
     fixture.detectChanges();
-    configService.tableData[0].sell = config.rates + (config.rates / 100 * 5);
-    expect (configService.tableData[0].sell).toEqual(1.05);
+    configService = fixture.debugElement.injector.get(ConfigService) ;
+    component.bases.sort((a, b) => a.curr.localeCompare(b.curr));
+    expect (component.bases[0].curr).toEqual('AUD');
+    component.bases.reverse();
+    expect (component.bases[0].curr).toEqual('USD');
+  });
+
+  it('should order Z-A', () => {
+    fixture = TestBed.createComponent(TableComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    configService = fixture.debugElement.injector.get(ConfigService) ;
+    component.bases.sort((a, b) => a.curr.localeCompare(b.curr));
+    component.bases.reverse();
+    expect (component.bases[0].curr).toEqual('USD');
   });
 
   it('should create', () => {
